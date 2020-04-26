941d47d4d25ab4a90eb72a99fb7a86fe0f1ba048
jcarrion <jcarrion@salesforce.com>
2020-04-26 20:11:33 +0200

    getRecordUi

--- force-app/main/default/lwc/jcvNewRecordForm/jcvNewRecordForm.js
+++ force-app/main/default/lwc/jcvNewRecordForm/jcvNewRecordForm.js
@@ -52,34 +52,40 @@ export default class JcvNewRecordForm extends LightningElement {
     rules;
 
      @wire(fetchRecordTypeValues, { objectName: '$objectApiName' })
-    getRecordTypes({ data, error }) {
-        if(data){
-             console.log(JSON.stringify(data));
-             this.jsonRt = data;
-             for(var rt in  data){
-                 if(this.recordTypeId == '')
-                 {
-                    this.recordTypeId =rt;
-                    this.getRtRules(data[rt]);
-                 }
-                 
-                console.log(rt);
-                console.log(data[rt]);
+     getRecordTypes({ data, error }) {
+        if(!this.recordId){
+            if(data){
+                console.log(JSON.stringify(data));
+                this.jsonRt = data;
+                for(var rt in  data){
+                    if(this.recordTypeId == '')
+                    {
+                        this.recordTypeId =rt;
+                        this.getRtRules(data[rt]);
+                    }
+                    
+                    console.log(rt);
+                    console.log(data[rt]);
 
-                var option = {};
-                option.label = data[rt];
-                option.value = rt;
-                this.listRt.push(option);
-             }
-             if(this.recordTypeId == '')
-             {
-                this.getRtRules('Master');
-             }
-             this.cargado = true;
-        }else if(error){
-            console.log('objectApiName es ' + this.objectApiName);
-            console.log(JSON.stringify(error));
+                    var option = {};
+                    option.label = data[rt];
+                    option.value = rt;
+                    this.listRt.push(option);
+                }
+                if(this.recordTypeId == '')
+                {
+                    this.getRtRules('Master');
+                }
+                this.cargado = true;
+            }else if(error){
+                console.log('objectApiName es ' + this.objectApiName);
+                console.log(JSON.stringify(error));
+            }
         }
+        else{
+            console.log('estamos en modo edit');
+        }
+
     }
 
 
@@ -130,10 +136,169 @@ export default class JcvNewRecordForm extends LightningElement {
     }
 
 
+    @wire(getRecordUi, { recordIds: '$recordIdCopy', layoutTypes: 'Full', modes: 'Edit'})
+    dcrRecordUi(response) {
+        var listLayout;
+        var LayoutSection;
+        var SectionList = [];
+        var FieldList = [];
+        this._response = response;
+        let error = response && response.error;
+        let data = response && response.data;
+
+        
+
+        if (data && this.hasRendered === false) {
+            console.log('Inicializando dcRecordUi...');
+
+            var record = data.records[this.recordId];
+
+            if(this.recordTypeInfo)
+            {
+                this.recordTypeId = record.recordTypeId;
+                console.log(this.recordTypeId);
+                var rtName = this.jsonRt[this.recordTypeId];
+                this.getRtRules(rtName);
+            }
+            else
+            {
+                console.log('getRtRules para master');
+                this.getRtRules('Master');
+            }
+
+
+            listLayout = data.layouts[this.objectApiName][Object.keys(data.layouts[this.objectApiName])].Full.Edit.sections;
+  
+            this.sectionsInForm = {};
+            for (let i = 0; i < listLayout.length; i++) {
+
+                var count = 0;
+  
+                LayoutSection = {};
+                FieldList = [];
+                LayoutSection.id = listLayout[i].id;
+                LayoutSection.heading = listLayout[i].heading;
+
+                let section = listLayout[i].heading;
+                this.sectionsInForm[section] = [];
+
+                for (let j = 0; j < listLayout[i].layoutRows.length; j++) {
+                    var boleanoColumnas = true;
+
+                    for (let k = 0; k < listLayout[i].layoutRows[j].layoutItems.length; k++) {
+                    
+                       
+                        var layoutItem = listLayout[i].layoutRows[j].layoutItems[k];
+                        
+                        for (let l = 0; l < layoutItem.layoutComponents.length; l++) {
+                            var layoutComponent = layoutItem.layoutComponents[l];
+                            var apiNamef = layoutComponent.apiName;
+                            
+                            let field = data.records[this.recordId].fields[apiNamef];
+                            if(field){
+                                var Field = {};
+                                
+                                Field = this.getField(layoutItem, layoutComponent);
+                                Field.fieldValue = field.value;
+                                this.formulario[apiNamef] ={"value": field.value, "seccion": i, "posicion":count};
+                                Field[apiNamef]={"fieldValue": field.value};
+                                count = count +1;
+                                Field.boleanoColumnas =boleanoColumnas;
+                                Field.key='0000'+i+j+k+l;
+                                boleanoColumnas = !boleanoColumnas;
+        
+                                this.sectionsInForm[section].push(apiNamef);
+                                FieldList.push(Field);
+
+                            }
+
+                        
+                        }
+                        
+
+                    }
+
+                }
+                LayoutSection.lstFields = FieldList;
+                SectionList.push(LayoutSection);
+                    
+            } 
+
+            if (SectionList) {
+                this.infoSectionsLayout = SectionList;
+            }
+            console.log('Finalizado dcRecordUi...');
+        }
     
+    }
 
 
+    getField(layoutItem, layoutComponent){
+        var Field = {};
+        var apiNamef = layoutComponent.apiName;
 
+        Field.apiName = apiNamef;
+        Field.ts = Date.now();
+        Field.isReadOnly = !layoutItem.editableForUpdate;
+        Field.isRequired = layoutItem.required;
+
+        Field.metadata = this.dataTypeAllFields.get(apiNamef);
+
+        if(Field.metadata.dataType == 'Address'){
+            Field.metadata.isAddress = true;
+        }
+        else if(Field.metadata.dataType == 'Base64')
+            Field.metadata.isBase64 = true;
+        else if(Field.metadata.dataType == 'Boolean')
+            Field.metadata.isBoolean = true;
+        else if(Field.metadata.dataType == 'ComboBox')
+            Field.metadata.isComboBox = true;
+        else if(Field.metadata.dataType == 'Currency')
+            Field.metadata.isCurrency = true;
+        else if(Field.metadata.dataType == 'Date')
+            Field.metadata.isDate = true;
+        else if(Field.metadata.dataType == 'DateTime')
+            Field.metadata.isDateTime = true;
+        else if(Field.metadata.dataType == 'Double')
+            Field.metadata.isDouble = true;
+        else if(Field.metadata.dataType == 'Email')
+            Field.metadata.isEmail = true;
+        else if(Field.metadata.dataType == 'Int')
+            Field.metadata.isInt = true;
+        else if(Field.metadata.dataType == 'Location')
+            Field.metadata.isLocation = true;
+        else if(Field.metadata.dataType == 'MultiPicklist')
+            Field.metadata.isMultiPicklist = true;
+        else if(Field.metadata.dataType == 'Percent')
+            Field.metadata.isPercent = true;
+        else if(Field.metadata.dataType == 'Phone')
+            Field.metadata.isPhone = true;
+        else if(Field.metadata.dataType == 'Picklist')
+        {
+            Field.metadata.isPicklist = true;
+        }
+        else if(Field.metadata.dataType == 'Reference')
+        {
+            Field.metadata.isReference = true;
+        } 
+        else if(Field.metadata.dataType == 'String')
+            Field.metadata.isString = true;
+        else if(Field.metadata.dataType == 'TextArea')
+            Field.metadata.isTextArea = true;
+        else if(Field.metadata.dataType == 'Time')
+            Field.metadata.isTime = true;
+        else if(Field.metadata.dataType == 'Url')
+        {
+            Field.metadata.isUrl = true;
+        }
+
+        Field.metadata.computedIsReadOnly = Field.isReadOnly;
+        Field.metadata.computedIsRequired = Field.isRequired;
+
+        return Field;
+
+
+    }
 
 
     getRtRules(recordType) {
